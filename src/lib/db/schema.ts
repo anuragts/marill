e14a4ed11c9adb51c821a064f595aea0e7515dd4
Anurag30112003 <anuragsharma011011@gmail.com>
2024-02-18 09:02:54 +0530

    feat: schema change

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -6,17 +6,23 @@ export const users = pgTable("users", {
   name: varchar("name"),
 });
 
-export const chats = pgTable("chats", {
-  id: serial("id").primaryKey(),
-  message: text("message").notNull(),
-  createdAt: text("created_at").notNull(),
-  userId: serial("user_id").references(() => users.id),
-});
-
 export const pdf = pgTable("pdf", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   chatId: serial("chat_id").references(() => chats.id),
   userId: serial("user_id").references(() => users.id),
   filePath: text("file_path").notNull(),
+});
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  createdAt: text("created_at").notNull(),
+  userId: serial("user_id").references(() => users.id),
+});
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  content: text("content").notNull(),
+  createdAt: text("created_at").notNull(),
+  chatId: serial("chat_id").references(() => chats.id),
 });
